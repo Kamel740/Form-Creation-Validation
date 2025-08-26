f697d2480510f835093e258259ed634a26f6be08
kamel740 <kamelelsayed740@gmail.com>
2025-08-26 12:20:14 +0300

    done first task

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+       const username = document.getElementById("username").value.trim();
+       const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+
+        let isValid = true;
+        let messages = [];
+        
+        if (username.length < 3){
+            isValid = false;
+            messages.push("error the name should be at least 3 charcter");
+        }
+
+        if (!email.includes("@")){
+            isValid = false;
+            messages.push("error email shoud contain @");
+        }
+
+        if (password.length < 8){
+            isValid = false;
+            messages.push("password should at least be 8 charcter")
+        }
+        
+        feedbackDiv.style.display = "block";
+        if (isValid){
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = '#28a745';
+        }
+        else {
+            feedbackDiv.innerHTML = messages.join("<br>")
+        }
+
+    });
+
+})
